0e4df08e41c6848a3354ae86b093ada991064aab
tarunshankar141 <23617141@vistas.ac.in>
2025-02-18 02:23:02 -0800

    Your commit message

--- python/divisible_by_5.py
+++ python/divisible_by_5.py
@@ -1,5 +1,10 @@
 def check_divisibility(num):
   # // Expected output is "TRUE" or "FALSE"
 
+  if num % 5 == 0:
+    print("TRUE")
+  else:
+    print("FALSE")  
+
 num = int(input("Enter a number: "))
 print(check_divisibility(num))
--- python/sum_function.py
+++ python/sum_function.py
@@ -1,5 +1,5 @@
 def sum_numbers(a, b):
-
+  return a+b 
 
 num1 = int(input("Enter first number: "))
 num2 = int(input("Enter second number: "))
